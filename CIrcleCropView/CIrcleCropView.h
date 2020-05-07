c96d98a8b44cd4ea06672875a364b17c6008d1df
bhavesh <bhavesh.chaudhari2015@gmail.com>
2020-05-07 11:10:19 +0530

    Setup Project

---
+++ CIrcleCropView/CIrcleCropView.h
@@ -0,0 +1,19 @@
+//
+//  CIrcleCropView.h
+//  CIrcleCropView
+//
+//  Created by Bhavesh Chaudhari on 07/05/20.
+//  Copyright © 2020 Bhavesh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CIrcleCropView.
+FOUNDATION_EXPORT double CIrcleCropViewVersionNumber;
+
+//! Project version string for CIrcleCropView.
+FOUNDATION_EXPORT const unsigned char CIrcleCropViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CIrcleCropView/PublicHeader.h>
+
+
---
+++ CropViewExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CropViewExample
+//
+//  Created by Bhavesh Chaudhari on 07/05/20.
+//  Copyright © 2020 Bhavesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
