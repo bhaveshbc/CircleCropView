12de330af7beacfaec8c854850887c335a2a8407
chaudahri bhavesh <bhavesh.iosdev@gmail.com>
2022-12-27 10:35:40 +0530

    update
    update

--- CIrcleCropView/CircleCropView.swift
+++ CIrcleCropView/CircleCropView.swift
@@ -16,6 +16,7 @@ public class CircleCropView: UIView {
         super.init(frame: frame)
         self.backgroundColor = UIColor.black.withAlphaComponent(0.58)
         isUserInteractionEnabled = false
+        isUserInteractionEnabled = true
     }
 
     required init?(coder aDecoder: NSCoder) {
